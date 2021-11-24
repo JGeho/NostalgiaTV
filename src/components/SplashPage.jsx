78a965608fbab1fe7dcbde39f23a8391dfe79c11
Gabriel Lopez <lopezg3000@gmail.com>
2021-11-23 18:27:58 -0600

    frontend server console errors fixed

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -5,9 +5,9 @@ export default function SplashPage() {
         <div>
             WELCOME TO NOSTALGIA TV
 
-            <a id="github-button" class="btn btn-block btn-social btn-github">
-                <i class="fa fa-github"></i> Sign in with GitHub
-            </a>
+            {/* <a id="github-button" className="btn btn-block btn-social btn-github">
+                <i className="fa fa-github"></i> Sign in with GitHub
+            </a> */}
         </div>
     )
 }
