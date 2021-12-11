eadc510ed885eff611b2c2de27f09b236d73a3ec
Gabriel Lopez <lopezg3000@gmail.com>
2021-12-11 13:19:00 -0600

    passport redirect added

--- src/components/splashPage/SplashPage.jsx
+++ src/components/splashPage/SplashPage.jsx
@@ -6,11 +6,15 @@ export default function Landingpage() {
   return (
     <Container id='landingPage'>
       <Row>
-        <Col className='leftCol'><a href="">
-          Left Side<br />
-          <img src="" height='' width='' /><br />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, sit aliquid doloribus suscipit deserunt impedit soluta laborum! Laudantium voluptate, fugiat nostrum sunt quas assumenda expedita veritatis atque voluptatem architecto ullam!
-        </a>
+        <Col className='leftCol'>
+          <div class="container">
+            <div class="jumbotron text-center text-primary">
+              <p>Login or Register with:</p>
+              <form method="GET" action="https://afternoon-stream-01263.herokuapp.com/auth/google">
+                <button class="btn btn-danger"><span class="fa fa-google"></span> SignIn with Google</button>
+              </form>
+            </div>
+          </div>
         </Col>
         <Col className='rightCol'>
           Right Side<br />
