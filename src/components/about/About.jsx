e337adaf93a2a821c016ed73a3e15b2c8cfa4cfd
JGeho <jgeho@hotmail.com>
2021-12-14 20:19:49 -0500

    about updates

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import justinpic from './JustinPic.jpg';
+import ericpic from './JustinPic.jpg';
+import gabepic from './JustinPic.jpg';
+
+// import { BsGithub } from 'react-icons/bs';
+// import { BsLinkedin } from 'react-icons/bs';
 
 export default function About() {
     return (
         <div>
             <h1>About Us</h1>
+
+            <h3>Eric Fricke</h3>
+                <img src={ericpic} alt="ericpic" className="ericpic" id="aboutpics"></img>
+                <a href="https://github.com/e-fricke" target="_blank" rel="noreferrer">Eric's GitHub</a>
+                <a href="https://www.linkedin.com/in/eric-fricke/" target="_blank" rel="noreferrer">Eric's LinkedIn</a>
+            <h3>Gabriel Lopez</h3>
+                <img src={gabepic} alt="gabepic" className="gabepic" id="aboutpics"></img>
+                <a href="https://github.com/lopezg3000" target="_blank" rel="noreferrer">Gabriel's GitHub</a>
+                <a href="https://www.linkedin.com/in/lopezgabrieljs/" target="_blank" rel="noreferrer">Gabriel's LinkedIn</a>
+            <h3>Justin Geho</h3>
+                <img src={justinpic} alt="justinpic" className="justinpic" id="aboutpics"></img>
+                <a href="https://github.com/JGeho" target="_blank" rel="noreferrer">Justin's GitHub</a>
+                <a href="https://www.linkedin.com/in/justin-geho/" target="_blank" rel="noreferrer">Justin's LinkedIn</a>
+
         </div>
     )
 }
+
